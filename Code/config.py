6df40d2690d43176f3003bdaa6b019308e0de4e0
Jodi H <59846195+jlheen@users.noreply.github.com>
2020-03-14 16:39:08 -0500

    Updated for project finalization.

--- Code/config.py
+++ Code/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7b00e33cdd592fc9991ed00d2f09a901"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCJQpW3cURSMK2bH2aoDc6Ka6Lh5HPUW7A"
